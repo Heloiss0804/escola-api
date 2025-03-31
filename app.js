d1d8995ecfe04671feb260724c1fdeaf990a6c05
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-03-31 11:57:14 -0300

    dar comando as ações

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { ler } from './src/aluno.js';
+import { ler, inserir, lerUm, excluir,atualizar} from './src/aluno.js';
 
 
 const app = express();
 const porta = 3000;
+
+//habilitando para dar suporte ao formato JSON
+app.use(express.json())
+
+// habilitando para dar suporte a dados inseridos a partir de inputs de formulário
+app.use(express.urlencoded({extended:true}));
 
 //Configurando Rotas
 
@@ -20,23 +26,41 @@ app.get('/alunos', (req, res) =>{
 
 //Exibindo UM aluno
 app.get('/alunos/:id', (req, res) =>{
-    res.send(`Exibindo de UM alunos`);
-})
+    //res.send(`Exibindo de UM alunos`);
+ 
+    //capturando o ID que vem do endpoint
+    const id = parseInt(req.params.id);
+
+    //chamar a função
+    lerUm(id, res);
+});
 
 //Inserindo/Cadastrando/Adicionando um aluno
 app.post('/alunos', (req, res) => {
-    res.send (`Inserir dados de UM aluno`);
+   // res.send (`Inserir dados de UM aluno`);
+
+   //capturar os dados a partir do corpo da requisição
+   const novoAluno = req.body;
+
+   //capturando a função inserir e passando os parâmetros novoAluno e res
+   inserir(novoAluno,res)
 });
 
 //Atualizando aluno
 app.patch('/alunos/:id', (req, res) =>{
-    res.send(`Atualizando dados do aluno`);
-})
+   // res.send(`Atualizando dados do aluno`);
+   
+});
 
 //Excluindo aluno
 app.delete('/alunos/:id', (req, res) =>{
-    res.send(`Aluno excluido com sucesso`);
-})
+   // res.send(`Aluno excluido com sucesso`);
+
+   // capturando o id
+   const id = parseInt(req.params.id);
+
+   excluir(id,res);
+});
 
 app.listen(porta, () => {
     console.log (`Servidor rodando na porta ${porta}`);
--- src/aluno.js
+++ src/aluno.js
@@ -7,6 +7,7 @@ function ler(res){
     //Seleciona todos os alunos, por ordem de todos os nomes
     const sql = "SELECT * FROM alunos ORDER BY nome";
 
+
     //Executando a query a partir da conexão
     conexao.query(sql,(erro,resultados)=>{
         
@@ -30,4 +31,69 @@ function ler(res){
    
 }
 
-export { ler };
+//Função para cadastrar os alunos no banco
+function inserir(aluno,res){
+   const sql = "INSERT INTO alunos SET ?";
+
+   conexao.query(sql,aluno, (erro) => {
+      if(erro){
+         res.status(400).json(erro.code);
+      } else {
+         res.status(201).json({"status" : "aluno inserido!"})
+      }    
+   });
+}
+
+//Função para exibir UM aluno
+function lerUm(id, res){
+   const sql = "SELECT * FROM alunos WHERE id = ?";
+
+   conexao.query(sql,id, (erro,resultados)=> {
+
+      if(resultados === 0){
+         res.status(204).end();
+         return;// forçar a interrupção do código
+      }
+
+       // If para erro ou resultado
+   if(erro){
+        res.status(400).json(erro.code);
+   }else {
+        res.status(200).json(resultados);
+   }
+
+   }) ;
+       //checando se há contéudo
+ 
+}
+
+// Função que exclui um aluno
+function excluir(id, res) {
+   const sql = "DELETE FROM alunos WHERE id = ?";
+
+   conexao.query(sql, id, (erro, resultados) => {
+      if (erro) {
+         res.status(400).json(erro.code);
+      } else {
+         res.status(200).json({ "status": "Aluno excluido!", id});
+      }
+   });
+}
+
+// Função para atualizar um aluno no banco
+function atualizar(id, aluno, res) {
+   const sql = "UPDATE alunos SET ? WHERE id = ?";
+
+   conexao.query(sql, [aluno, id], (erro, resultados) => {
+      if (erro) {
+         res.status(400).json(erro.code);
+      } else {
+         res.status(200).json({ "status": "Aluno atualizado com sucesso!" });
+      }
+   });
+}
+
+
+
+
+export { ler, inserir, lerUm, excluir, atualizar};
